4eb9f21281bd7a2063662c823eb84b89487a54bc
Gin641 <53481351+Gin641@users.noreply.github.com>
2020-04-14 17:51:03 +0700

    hieu suong suong

---
+++ src/Test.java
@@ -0,0 +1,15 @@
+public class Test {
+    public static void main(String[] args) {
+        LazyPrime lazyPrime = new LazyPrime();
+        OptimizedPrime optimizedPrime = new OptimizedPrime();
+        Thread thread1 = new Thread(lazyPrime);
+        Thread thread2 = new Thread(optimizedPrime);
+        thread1.start();
+        try {
+            thread1.join();
+        }catch (InterruptedException ex){
+            System.out.println(ex.getMessage());
+        }
+        thread2.start();
+    }
+}
